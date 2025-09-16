2f55f1c2fd85e5f0eacb89bcb8f16b00c9b760c3
idutta999 <indrajitdutta50@gmail.com>
2025-09-16 14:49:57 +0200

    Update path to app

--- cypress/e2e/sample.cy.js
+++ cypress/e2e/sample.cy.js
@@ -1,6 +1,6 @@
 describe('example tests', () => {
     it('Should have a title', () => {
-        cy.visit('app/index.html')
+        cy.visit('counter-e2e-test/app/index.html')
         cy.get('button[id="increment-btn"]')
             .should('be.enabled')
     })
